1c044464afa1b132ba0a48b67183a148bfb5c57d
fran0436 <fran0436@algonquinlive.com>
2017-12-22 18:41:33 -0500

    first commit
    first commit

---
+++ starter.js
@@ -0,0 +1,17 @@
+let contactStarter = [
+    {
+        "fullname": "Maurice Moss",
+        "phone": "0118-999-881-999-119-7253",
+        "email": "maurice.moss@itreynholm.com"
+    },
+    {
+        "fullname": "Roy Trenneman",
+        "phone": "613-222-6666",
+        "email": "haveyoutriedturningitoffandonagain@itreynholm.com"
+    },
+    {
+        "fullname": "Jen Barber",
+        "phone": "613-234-1234",
+        "email": "TAXI@itreynholm.com"
+    }
+];
